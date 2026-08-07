5f9469dd4025883a1dc84d57044fabff95e83603
illustager <illustager@outlook.com>
2026-08-07 19:10:33 +0800

    feat: mallory 输出密钥第一字节采样结果

--- e/pp/pp.h
+++ e/pp/pp.h
@@ -1,7 +1,9 @@
 #include <stdint.h>
-
+#include <stdbool.h>
 
 typedef void (*crypto_fn)(uint8_t*, uint8_t*, void*);
+
+extern bool FOUT;
 
 void pp_init(unsigned cache_sets, unsigned cache_line_shift, unsigned cache_level);
 uint8_t pp(crypto_fn crypto, void *data, unsigned samples, unsigned idx);
